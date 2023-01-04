8bbb13cc618ec96b5c557af177c05ffd0d80ba93
Tilio77 <tiliobolso1@gmail.com>
2023-01-04 19:22:24 -0300

    Contador terminado

--- src/components/pages/Counter.jsx
+++ src/components/pages/Counter.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { project } from '../data/dummydata';
+import CountUp from 'react-countup';
 
 const Counter = () => {
 	return (
@@ -9,7 +10,9 @@ const Counter = () => {
 					{project.map((item,i) => (
 						<div className="box">
 							<i>{item.icon}</i>
-							<h1 className="heading">{item.title}</h1>
+							<h1 className="heading">
+								<CountUp enableScrollSpy duration={2} end={item.num} />
+							</h1>
 							<h3>{item.title}</h3>
 						</div>
 					))}
