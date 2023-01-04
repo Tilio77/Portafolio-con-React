d577ff00ee2520af07bb88b8c3431aee78a401a6
Tilio77 <tiliobolso1@gmail.com>
2023-01-04 18:00:08 -0300

    Navbar terminado y responsivo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Pages from './components/pages/Pages';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/common/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <>
-      <Header />
       <Router>
+      <Header />
         <Switch>
-          
+          <Route exact path="/" component={Home} />
         </Switch>
       </Router>
     </>
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { navlink } from "../data/dummydata";
+import logo from '../data/images/logo.png';
+import { Menu } from '@mui/icons-material';
 
 const Header = () => {
+
+	const [responsive, setResponsive] = useState(false);
+
 	return (
 		<>
 			<header>
 				<div className="container flexsb">
 					<div className="logo">
-						<img src={logo} />
+						<img src={logo} alt='' />
 					</div>
-					<div className="nav">
-						<Link to=""></Link>
+						<div className={responsive ? "hideMenu" : 'nav'}>{navlink.map((links, i) => (
+							<Link 
+								to={links.url} 
+								key={i}
+							>
+								{links.text}
+							</Link>
+						))}
 					</div>
+					<button className="toggle" onClick={() => setResponsive(!responsive)}>
+						<Menu className="icon"></Menu>
+					</button>
 				</div>
 			</header>
 		</>
