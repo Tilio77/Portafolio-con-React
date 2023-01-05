95df5a9f7b64deb2192bd2839a9125e348db21c5
Tilio77 <tiliobolso1@gmail.com>
2023-01-04 23:28:43 -0300

    Movimiento de archivos

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import Home from './components/home/Home';
-import About from './components/pages/About';
-import Services from './components/pages/Services';
-import Portfolio from './components/pages/Portfolio';
-import Testimonials from './components/pages/Testimonials';
-import Blog from './components/pages/Blog';
-import Contact from './components/pages/Contact';
+import "./App.css"
+import Pages from "./components/pages/Pages"
+import { useEffect } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 function App() {
+  //aos
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh()
+  }, [])
   return (
     <>
-      <Router>
-      <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/services" component={Services} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/testimonials" component={Testimonials} />
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      <Footer />
-      </Router>
+      <Pages />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+import Home from '../home/Home';
+import About from './About';
+import Services from './Services';
+import Portfolio from './Portfolio';
+import Testimonials from './Testimonials';
+import Blog from './Blog';
+import Contact from './Contact';
 
-const Pages = () => {
-	return (
-		<>
-			Pages
-		</>
-	)
+function App() {
+  return (
+    <>
+      <Router>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/testimonials" component={Testimonials} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      <Footer />
+      </Router>
+    </>
+  );
 }
 
-export default Pages
+export default App;
